577a2b6f66214259e696c4fb106dd1e719c9fa98
Annams02 <155559840+Annams02@users.noreply.github.com>
2024-05-23 15:30:58 +0000

    favs finished

--- src/models.py
+++ src/models.py
@@ -7,23 +7,43 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):   
+    __tablename__ = 'user' 
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    nombre = Column(String(20))
+    email = Column(String(30))
+    password = Column(String(30))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planetas(Base):   
+    __tablename__ = 'planetas' 
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    nombre = Column(String(30))
+    population = Column(Integer)
+    gravity = Column(String(30))
+
+class Personajes(Base):    
+    __tablename__ = 'personajes'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(30))
+    estatura = Column(Integer)
+    colorOjos = Column(String(30))
+
+class Favoritos_Pl(Base):
+    __tablename__ = 'favoritos_PL'
+    idFav = Column(Integer, primary_key= True)  
+    id_pj = Column(Integer,ForeignKey('Planetas.id'))
+    Planetas = relationship('Planetas')
+    id_user = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+class Favoritos_Pj(Base):
+    __tablename__ = 'favoritos_Pj'
+    idFav = Column(Integer, primary_key= True)
+    id_pj = Column(Integer,ForeignKey('Personajes.id'))
+    Personajes = relationship('Personajes')
+    id_user = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     def to_dict(self):
         return {}
